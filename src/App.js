7cf137db98f972743d1c864c28adf50099c2bc39
ikramKOU <104200410+ikramKOU@users.noreply.github.com>
2023-09-15 11:36:25 +0100

    hi
    hello

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
-
-import './App.css';
-import Login from './components/Login/Login'
-import Register from './components/Register/Register'
-import Landing_pg from './components/Land/Landing_pg'
+import './App.css' 
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Landing_pg from './components/Land/Landing_pg';
+import Edit from './components/Ed_profil/Edit';
+import Profil from './components/Profil_card/Profil';
 
 
 
@@ -17,6 +18,10 @@ function App() {
   <Route  path="/" element={<Login />} />
   <Route path="register" element={<Register />} />
   <Route path="lnd_pg" element={<Landing_pg />} />
+  <Route path="edit" element={<Edit />} />
+  <Route path="profil_card" element={<Profil />} />
+
+
 
   <Route path="*" element={<h1>404</h1>} />
 </Routes>
